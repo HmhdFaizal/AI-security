01329d1e9e5e8c40811c57449acbb877a7be2d56
Hmhdfaizal <hmuhammedfaizal22@gmail.com>
2026-02-01 19:00:49 +0530

    cross platform update

--- ai_security.py
+++ ai_security.py
@@ -1,10 +1,13 @@
 import psutil
 import time
+import platform
 
-print("\nAI Security Tool – Learning Mode\n")
+os_name = platform.system().lower()
+
+print("\nAI Security Tool – Learning Mode({os_name.upper()})\n")
 print("Recording normal behavior...\n")
 
-log_file = "behavior_log.txt"
+log_file = "behavior_log-{os_name}.txt"
 
 with open(log_file, "a") as log:
     for process in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent']):
